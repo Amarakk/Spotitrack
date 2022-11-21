c717a10a641529b832b5402eb8f489a59f5466f7
Arthur <arthur.souza@anota.ai>
2022-11-21 11:00:50 -0300

    heroku config

---
+++ Spotitrack/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static('./dist/app-heroku'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/app-heroku/'}),
+);
+app.listen(process.env.PORT || 8080);
